d3fd66fdc80d8f8c446ca6b26f867d89dc4555c3
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-01 13:51:36 -0300

    Modificar menú de opciones
    Se agrega opción salir, do while para que el menú vuelva a pedir datos y también método para limpiar la pantalla.

--- PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
@@ -16,65 +16,78 @@ namespace Clase1.Ejercicio1.Figuras
         {
             int sel, lado, bas, alt, l1, l2, l3;
 
-            List<int> datos= new List<int>(); ;
-
-            Console.WriteLine("--------Bienvenido al programa de cálculo de Perímetro y Área de Figuras--------");
-            Console.WriteLine("Seleccione una figura: ");
-            Console.WriteLine("1. Cuadrado");
-            Console.WriteLine("2. Rectángulo");
-            Console.WriteLine("3. Triángulo");
-            sel = Convert.ToInt32(Console.ReadLine());
-
-            switch (sel)
+            do
             {
-                case 1:
-                    Console.WriteLine("Ingrese el lado del Cuadrado: ");
-                    lado = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(lado);
+                Console.Clear();
+                List<int> datos = new List<int>(); ;
 
-                    Cuadrado c = new Cuadrado(datos);
-          
-                    Console.WriteLine("El Perímetro del Cuadrado es " + c.calcularPerimetro());
-                    Console.WriteLine("El Área del Cuadrado es " + c.calcularArea());
-                    Console.Read();
+                Console.WriteLine("--------Bienvenido al programa de cálculo de Perímetro y Área de Figuras--------");
+                Console.WriteLine("Seleccione una figura: ");
+                Console.WriteLine("1. Cuadrado");
+                Console.WriteLine("2. Rectángulo");
+                Console.WriteLine("3. Triángulo");
+                Console.WriteLine("4. Salir");
+                sel = Convert.ToInt32(Console.ReadLine());
+                
+                switch (sel)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el lado del Cuadrado: ");
+                        lado = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(lado);
 
-                    break;
-                case 2:
-                    Console.WriteLine("Ingrese la base del Rectángulo: ");
-                    bas = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(bas);
-                    Console.WriteLine("Ingrese la altura del Rectángulo: ");
-                    alt = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(alt);
+                        Cuadrado c = new Cuadrado(datos);
 
-                    Rectangulo r = new Rectangulo(datos);
+                        Console.WriteLine("El Perímetro del Cuadrado es " + c.calcularPerimetro());
+                        Console.WriteLine("El Área del Cuadrado es " + c.calcularArea());
+                        //Console.Read();
 
-                    Console.WriteLine("El Perímetro del Rectángulo es " + r.calcularPerimetro());
-                    Console.WriteLine("El Área del Rectángulo es " + r.calcularArea());
-                    Console.Read();
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese la base del Rectángulo: ");
+                        bas = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(bas);
+                        Console.WriteLine("Ingrese la altura del Rectángulo: ");
+                        alt = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(alt);
 
-                    break;
-                case 3:
-                    Console.WriteLine("Ingrese el lado 1 del Triángulo: ");
-                    l1 = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(l1);
+                        Rectangulo r = new Rectangulo(datos);
 
-                    Console.WriteLine("Ingrese el lado 2 del Triángulo: ");
-                    l2 = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(l2);
+                        Console.WriteLine("El Perímetro del Rectángulo es " + r.calcularPerimetro());
+                        Console.WriteLine("El Área del Rectángulo es " + r.calcularArea());
+                       // Console.Read();
 
-                    Console.WriteLine("Ingrese el lado 3 del Triángulo: ");
-                    l3 = Convert.ToInt32(Console.ReadLine());
-                    datos.Add(l3);
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese el lado 1 del Triángulo: ");
+                        l1 = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(l1);
 
-                    Triangulo t = new Triangulo(datos);
+                        Console.WriteLine("Ingrese el lado 2 del Triángulo: ");
+                        l2 = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(l2);
 
-                    Console.WriteLine("El Perímetro del Triángulo es " + t.calcularPerimetro());
-                    Console.WriteLine("El Área del Triángulo es " + t.calcularArea());
-                    Console.Read();
+                        Console.WriteLine("Ingrese el lado 3 del Triángulo: ");
+                        l3 = Convert.ToInt32(Console.ReadLine());
+                        datos.Add(l3);
 
-                    break;
-            }
+                        Triangulo t = new Triangulo(datos);
+
+                        Console.WriteLine("El Perímetro del Triángulo es " + t.calcularPerimetro());
+                        Console.WriteLine("El Área del Triángulo es " + t.calcularArea());
+                       // Console.Read();
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("La opción ingresada no es correcta.");
+                        break;
+                }
+
+                Console.ReadKey();
+            } while (sel != 4);
         }
     }
 }
