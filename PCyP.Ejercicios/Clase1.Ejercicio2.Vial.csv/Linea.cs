1bf3d8f5baa32c6f577eac49f834f4ae7e205ac6
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-01 10:34:23 -0300

    Crear clase Linea
    Se crea la clase encargada de registrar campos e imprimirlos.

---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial.csv/Linea.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial.csv
+{
+    class Linea
+    {
+        //atributos
+        private int numHoja { get; set; }
+        private string tipoObj { get; set; }
+        private long idTramo { get; set; }
+        private string tipoCamino { get; set; }
+        private int longitud { get; set; }
+
+        //constructores
+        public Linea()
+        {
+            numHoja = 0;
+            tipoObj = null;
+            idTramo = 0;
+            tipoCamino = null;
+            longitud = 0;
+        }
+
+        public Linea(int nHoja, string tObj, long idT, string tCamino, int longd)
+        {
+            numHoja = nHoja;
+            tipoObj = tObj;
+            idTramo = idT;
+            tipoCamino = tCamino;
+            longitud = longd;
+        }
+
+        //método para imprimir los campos de la línea.
+        public void imprimir()
+        {
+          
+        }
+    }
+}
