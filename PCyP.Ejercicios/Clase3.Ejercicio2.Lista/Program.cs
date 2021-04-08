57f4f7e959365c7cfc4a2c7b0f6490526447c3e9
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-08 13:44:03 -0300

    Implementar Ejercicio2
    Desarrollar los puntos del ejercicio 2 sobre manejo básico de listas

--- PCyP.Ejercicios/Clase3.Ejercicio2.Lista/Program.cs
+++ PCyP.Ejercicios/Clase3.Ejercicio2.Lista/Program.cs
@@ -8,8 +8,7 @@ namespace Clase3.Ejercicio2.Lista
 {
     /// <summary>
     /// Dado dos vectores:
-    /// private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
-    /// private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+    /// 
     /// Realice las siguientes operaciones:
     /// a.Armar dos listas, una con los valores del vector “colors” y otra con los valores del
     /// vector “removeColors”.
@@ -19,10 +18,64 @@ namespace Clase3.Ejercicio2.Lista
     /// </summary>
     class Program
     {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo.Lista!");
+            List<string> colores = new List<string>();
+            List<string> coloresAremover = new List<string>();
+
+            // punto a 
+            foreach (var color in colors)
+            {
+                colores.Add(color);
+            }
+
+            foreach (var color in removeColors)
+            {
+                coloresAremover.Add(color);
+            }
+
+            //punto b 
+            mostrarLista(colores);
+
+            //punto c
+            eliminarColores(colores, coloresAremover);
+
+            //punto d
+            Console.WriteLine("Lista después de llamar a remueve colores: ");
+            mostrarLista(colores);
+
             Console.Read();
         }
+
+         
+        private static void mostrarLista(List<string> lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine("Tamaño:" +lista.Count);         
+        }
+
+        
+        private static void eliminarColores(List<string> original, List<string> eliminar)
+        {
+            for (int contador = 0; contador < eliminar.Count; contador++)
+            {
+                original.Remove(eliminar[contador]);
+            }
+        }
+
+
     }
 }
+   
+
+
+
+
+
