465d34ca4d6c3dab906660b48245d4e63a0adb32
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-08 17:02:27 -0300

    Implementar Ejercicio3
    Desarrollar contador de pablaras.

--- PCyP.Ejercicios/Clase3.Ejercicio3.ContadorPalabras/Program.cs
+++ PCyP.Ejercicios/Clase3.Ejercicio3.ContadorPalabras/Program.cs
@@ -15,7 +15,61 @@ namespace Clase3.Ejercicio3.ContadorPalabras
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo.Contador");
+            Dictionary <string, int> diccionario = new Dictionary <string, int>();
+
+            int contador = 0;
+
+            Console.WriteLine("Ingrese un párrafo: ");
+
+            string[] parrafo = Console.ReadLine().Split(' ');
+
+
+            foreach (string palabra in parrafo)
+            {
+                 foreach (string palabrarepetida in parrafo)
+                {
+                   if (palabra.Equals(palabrarepetida))
+                  {
+                     contador++;
+                  }
+                }
+                
+                if (diccionario.Count == 0)
+                {
+                    diccionario.Add(palabra, contador);
+                }
+                else
+                {
+                    bool repetido = false;
+
+                    for (int i = 0; i < diccionario.Count; i++)
+                    {
+                        if (diccionario.ElementAt(i).Key.Equals(palabra) && diccionario.ElementAt(i).Value.Equals(contador))
+                        {
+                            repetido = true;
+                            break;
+                        }
+                    }
+                    if (repetido == false)
+                    {
+                        diccionario.Add(palabra, contador);
+                    }
+                }
+                contador = 0;
+            }
+
+            Console.WriteLine("Contenido del diccionario: ");
+            
+            foreach (KeyValuePair<string, int> elemento in diccionario)
+            {
+                Console.WriteLine(elemento.Key + " " + elemento.Value);
+                contador++;
+            }
+
+            Console.WriteLine("Tamaño del diccionario: " + contador);
+
+            Console.WriteLine("Presione enter para salir . . ");
+
             Console.Read();
         }
     }
