4db346601f39b5b4098d6b37d7caa9cdf8ef43c4
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-08 11:16:20 -0300

    Crear Proyectos
    Creación de los proyectos de la clase 3

---
+++ PCyP.Ejercicios/Clase3.Ejercicio2.Lista/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio2.Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo.Lista!");
+            Console.Read();
+        }
+    }
+}
---
+++ PCyP.Ejercicios/Clase3.Ejercicio4.Netflix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio4.Netflix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo.Netflix");
+            Console.Read();
+        }
+    }
+}
