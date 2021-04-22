7d98c6560811dc6b0dde01e2c89edc9748fabf86
JessSena <73967376+JessSena@users.noreply.github.com>
2021-04-22 16:50:15 -0300

    Implementar Ejercicio Matrices
    Realizar el ejercicio

--- PCyP.Ejercicios/Clase5.EjercicioMatrices/Program.cs
+++ PCyP.Ejercicios/Clase5.EjercicioMatrices/Program.cs
@@ -10,6 +10,93 @@ namespace Clase5.EjercicioMatrices
     {
         static void Main(string[] args)
         {
-        }
+                Console.WriteLine("-----Bienvenido al programa de Multiplicación de matrices-----");
+
+                //pidiendo datos para la primer matriz 
+                Console.WriteLine("\n Primer Matriz");
+
+                Console.Write("Ingrese la cantidad de Filas: ");
+                int f1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la cantidad de Columnas: ");
+                int c1 = int.Parse(Console.ReadLine());
+
+                //pidiendo datos para la segunda matriz 
+                Console.WriteLine("\n Segunda matriz");
+
+                Console.Write("Ingrese la cantidad de Filas: ");
+                int f2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la cantidad de Columnas: ");
+                int c2 = int.Parse(Console.ReadLine());
+
+                //Declarando las matrices 
+
+                int[,] primerMatriz = new int[f1 + 1, c1 + 1];
+                int[,] segundaMatriz = new int[f2 + 1, c2 + 1];
+                int[,] resultado = new int[f1 + 1, c2 + 1];
+
+
+                //Las columnas de la matriz 1 deben ser igual al número de filas de la matriz 2
+                if (c1 == f2)
+                {
+                    Console.WriteLine(" \n Las matrices son multiplicables");
+                    
+                    //Rellenando primer matriz
+                    Console.WriteLine(" \n Datos Primer Matriz: ");
+                    for (int i = 1; i <= f1; i++)
+                    {
+                        for (int j = 1; j <= c1; j++)
+                        {
+                            Console.Write("Ingrese Fila: {0} - Columna: {1}: ", i, j);
+                            primerMatriz[i, j] = int.Parse(Console.ReadLine());
+                        }
+                    }
+
+                    //Rellenando segunda matriz
+                    Console.WriteLine("\n Datos Segunda Matriz: ");
+                    for (int i = 1; i <= f2; i++)
+                    {
+                        for (int j = 1; j <= c2; j++)
+                        {
+                            Console.Write("Ingrese Fila: {0} - Columna: {1}: ", i, j);
+                            segundaMatriz[i, j] = int.Parse(Console.ReadLine());
+                        }
+                    }
+
+                    //Multiplicando las matrices
+                    for (int i = 1; i <= f1; i++)
+                    {
+                        for (int j = 1; j <= c2; j++)
+                        {
+                            resultado[i, j] = 0;
+                            for (int z = 1; z <= c1; z++)
+                            {
+                                resultado[i, j] = primerMatriz[i, z] * segundaMatriz[z, j] + resultado[i, j];
+                            }
+                        }
+                    }
+
+
+                    Console.WriteLine("\n Matriz resultante de la multiplicación de las 2 Matrices: \n");
+
+                    // mostramos el resultado
+                    for (int i = 1; i <= f1; i++)
+                    {
+                        for (int j = 1; j <= c2; j++)
+                        {
+                            Console.Write("{0} ", resultado[i, j]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n No se puede multiplicar las matrices " +
+                        "\n Columnas primer Matriz : {0} , Filas segunda Matriz : {1} \n Las mismas deben ser iguales ", c1, f2);
+                }
+                Console.Read();
+         }
+            
     }
 }
