ef810675bc2019fea6c19c1b08ff23b32f9490ba
JessSena <73967376+JessSena@users.noreply.github.com>
2021-06-17 20:18:35 -0300

    Implementación del ejercicio

--- PCyP.Ejercicios/Clase9.WaitAllOneByOne/Program.cs
+++ PCyP.Ejercicios/Clase9.WaitAllOneByOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clase9.WaitAllOneByOne
@@ -11,7 +12,63 @@ namespace Clase9.WaitAllOneByOne
     {
         static void Main(string[] args)
         {
-            
+            var sw = Stopwatch.StartNew();
+
+            sw.Start();
+
+            Task<int> t1 = new Task<int>(() =>{
+                Thread.Sleep(2000);
+                Console.WriteLine("Calculando el resultado de t1");
+
+                return 500;
+               });
+
+            Task<int> t2 = new Task<int>(() =>
+              {
+                  Thread.Sleep(1000);
+                  Console.WriteLine("Calculando el resultado de t2");
+                  return 200;
+              });
+
+            Task<int> t3 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Calculando el resultado de t3");
+                return 30;
+            });
+
+            Task<int> t4 = new Task<int>(() =>
+            {
+  
+                Console.WriteLine("Calculando el resultado de t4");
+                return 15;
+            });
+
+            var taskList = new List<Task<int>>();
+            taskList.Add(t1);
+            taskList.Add(t2);
+            taskList.Add(t3);
+            taskList.Add(t4);
+
+            taskList.ForEach(task => task.Start());
+
+            Console.WriteLine("Main en progreso");
+
+            while (taskList.Count > 0)
+            {
+                var index = Task.WaitAny(taskList.ToArray());
+                Console.WriteLine("El resultado de la tarea " + taskList[index].Id + " es: "+taskList[index].Result);
+                taskList.RemoveAt(index);
+            }
+
+            Console.WriteLine("fin del Main.");
+
+            sw.Stop();
+
+            Console.WriteLine("The used time was {0} seconds", (sw.ElapsedMilliseconds / 1000));
+            Console.ReadLine();
+
         }
+
     }
 }
